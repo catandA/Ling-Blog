4dc23280f5f6f1e1aa0b9fe14f1fe708d3e8d071
LingASDJ <2735951230@qq.com>
2022-05-18 19:05:19 +0800

    Update time.js

--- assets/js/time.js
+++ assets/js/time.js
@@ -4,6 +4,7 @@ function siteTime() {
   var minutes = seconds * 60;
   var hours = minutes * 60;
   var days = hours * 24;
+  var years = days * 365;
   var today = new Date();
   var todayYear = today.getFullYear();
   var todayMonth = today.getMonth() + 1;
@@ -14,6 +15,7 @@ function siteTime() {
   var t1 = Date.UTC(2022, 05, 17, 16, 00, 00);
   var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
   var diff = t2 - t1;
+  var diffYears = Math.floor(diff / years);
   var diffDays = Math.floor((diff / days) - diffYears * 365);
   var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
   var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
